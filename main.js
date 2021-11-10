0f36c8d74c1c85ccf9affc03120a4523b3945af3
Igor Wiepieski <iw.programacao@gmail.com>
2021-11-10 11:03:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const linksSocialMedia = {
+  github: 'iwprogramacao',
+  instagram: 'iw.programacao',
+  youtube: 'watch?v=dQw4w9WgXcQ',
+  twitter: 'xetdoifood',
+  facebook: 'zuck'
+}
+
+function changeSocialMediaLinks() {
+  document.getElementById('userName').textContent = 'Igor Wiepieski'
+  userName.textContent = 'Igor Wiepieski'
+  /* Os dois itens significam a mesma coisa (foco no textContent) */
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGit.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
